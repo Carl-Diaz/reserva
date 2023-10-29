f6848c70666fd352d6d153e5ff37257cb3b1efda
alberto diaz <albersan338@gmail.com>
2023-10-29 17:31:38 -0500

    inicio de app de reserva

---
+++ reserva.js
@@ -0,0 +1,8 @@
+let nombreCompleto = "Ada Lovelace";
+let numeroTelefono = 78034288282;
+let fechaReserva = "2023-10-30";
+let horaReserva = "19:00";
+let numeroPersonas = 6;
+
+const CAPACIDAD_MAXIMA_PERSONAS = 50;
+const DESCUENTO_GRUPO_GRANDE = 10;
